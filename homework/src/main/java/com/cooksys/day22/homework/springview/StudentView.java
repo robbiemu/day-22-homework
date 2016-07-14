85f53f6f58db25ad16c88f89b2e095ce2268ea46
robbiemu <roberto.tomas.cuentas@gmail.com>
2016-07-14 13:53:13 -0400

    added city name to results

--- homework/src/main/java/com/cooksys/day22/homework/springview/StudentView.java
+++ homework/src/main/java/com/cooksys/day22/homework/springview/StudentView.java
@@ -5,12 +5,14 @@ import com.cooksys.day22.homework.model.Student;
 public class StudentView {
 	Long id;
 	String name;
+	String city;
 	
 	public StudentView () { }
 	
 	public StudentView (Student student) {
 		this.name = student.getName();
 		this.id = student.getId();
+		this.city = student.getCity().getName();
 	}
 	
 	public Long getId() {
@@ -24,6 +26,12 @@ public class StudentView {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
 	}
 	
 }
